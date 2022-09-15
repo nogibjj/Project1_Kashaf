5a4c541837b0dbbbe9a38afa2249385038aaa4b1
kashafali8 <111402572+kashafali8@users.noreply.github.com>
2022-09-15 20:50:10 +0000

    format

--- main_app.py
+++ main_app.py
@@ -4,10 +4,11 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {
-        "message": "Hello folks! Let's learn about Spotify Top50 charts"}
+    return {"message": "Hello folks! Let's learn about Spotify Top50 charts"}
+
 
 @app.get("/add/{num1}/{num2}")
 async def add(num1: int, num2: int):
@@ -16,10 +17,11 @@ async def add(num1: int, num2: int):
     total = num1 + num2
     return {"total": total}
 
+
 @app.get("/charts/{title}")
 async def read_item(title):
     return {"title": title}
 
+
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
-
